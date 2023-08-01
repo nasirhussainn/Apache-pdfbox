25a9321f45fd5593ed64fda6844c3d3de2915803
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-08-01 20:30:50 +0500

    Add files via upload
    modify existing pdf

--- CreatePDF.java
+++ CreatePDF.java
@@ -2,17 +2,25 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 
 import java.io.IOException;
+import java.io.File;
 
 public class CreatePDF {
     public static void main(String[] args) throws IOException{
-        PDDocument document = new PDDocument();
-        PDPage firstPage=new PDPage();
+//        PDDocument document = new PDDocument();
+//        PDPage firstPage=new PDPage();
+//
+//        firstPage.getContents();
+//        document.addPage(firstPage);
+//
+//        document.save("E:\\Project Practice\\APACHE PDFBOX\\PracticeTutorial\\newPDF.pdf");
+//        System.out.println("PDF created successfully");
+//        document.close();
+        File oldfile=new File("E:\\Project Practice\\APACHE PDFBOX\\PracticeTutorial\\newPDF.pdf");
+        PDDocument document=PDDocument.load(oldfile);
+        document.addPage(new PDPage());
 
-        firstPage.getContents();
-        document.addPage(firstPage);
-
-        document.save("E:\\Project Practice\\APACHE PDFBOX\\PracticeTutorial\\newPDF.pdf");
-        System.out.println("PDF created successfully");
+        document.save("E:\\Project Practice\\APACHE PDFBOX\\PracticeTutorial\\newPDF2.pdf");
+        System.out.println("New PDF created");
         document.close();
     }
 }
