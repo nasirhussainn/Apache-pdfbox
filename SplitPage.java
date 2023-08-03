70eb15e30d2f8278d3e050e092ecacd52f1d5afc
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-08-03 18:58:38 +0500

    Add files via upload
    --split in range

--- SplitPage.java
+++ SplitPage.java
@@ -7,19 +7,39 @@ import java.util.List;
 
 public class SplitPage {
     public static void main(String[] args) throws IOException{
+//        File oldfile=new File("E:\\Project Practice\\APACHE PDFBOX\\SplitPages\\nasir.pdf");
+//        PDDocument document = PDDocument.load(oldfile);
+//
+//        Splitter splitter=new Splitter();
+//
+//        List<PDDocument> splitPages=splitter.split(document);
+//
+//        int num=1;
+//        for(PDDocument mydoc : splitPages){
+//            mydoc.save("E:\\Project Practice\\APACHE PDFBOX\\SplitPages\\split_0"+num+".pdf");
+//            num++;
+//            mydoc.close();
+//        }
+//       System.out.println("Splitting Document successfully");
         File oldfile=new File("E:\\Project Practice\\APACHE PDFBOX\\SplitPages\\nasir.pdf");
         PDDocument document = PDDocument.load(oldfile);
 
+        File newFileDestination= new File("E:\\Project Practice\\APACHE PDFBOX\\SplitPages");
+        newFileDestination.mkdirs();
+//        the above if directory not exist
+
         Splitter splitter=new Splitter();
+        splitter.setStartPage(2);
+        splitter.setEndPage(5);
 
         List<PDDocument> splitPages=splitter.split(document);
 
-        int num=1;
+        PDDocument newDoc= new PDDocument();
         for(PDDocument mydoc : splitPages){
-            mydoc.save("E:\\Project Practice\\APACHE PDFBOX\\SplitPages\\split_0"+num+".pdf");
-            num++;
-            mydoc.close();
+            newDoc.addPage(mydoc.getPage(0));
         }
+        newDoc.save("E:\\Project Practice\\APACHE PDFBOX\\SplitPages\\range.pdf");
+        newDoc.close();
         System.out.println("Splitting Document successfully");
     }
 }
