f40f27e0582d0fba8bd4f5976f1348ae481ba3b2
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-08-07 18:40:21 +0500

    Add files via upload

--- MergePages.java
+++ MergePages.java
@@ -21,6 +21,16 @@ public class MergePages {
 
         merge.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
 
+        PDDocument document1=PDDocument.load(oldFile1);
+        PDDocument document2=PDDocument.load(oldFile2);
+
+        for(int i=0;i<document2.getNumberOfPages();i++){
+            document1.addPage(document2.getPage(i));
+        }
+        document1.save(newFile+"\\loop.pdf");
+
+
+
 //        System.out.println("PDF Created after deletion");
         System.out.println("PDF Created after Merge");
     }
