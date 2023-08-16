75fd1522d848bb8af055f6de9f2938942ee7c8a5
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-08-16 16:16:24 +0500

    Add files via upload
    --add image content

--- WritePDF.java
+++ WritePDF.java
@@ -3,6 +3,8 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImage;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import java.awt.*;
 import java.io.IOException;
@@ -14,6 +16,7 @@ public class WritePDF {
         PDPage firstPage = new PDPage();
         document.addPage(firstPage);
 
+//        To add text content in pdf
         PDPageContentStream pdPageContentStream = new PDPageContentStream(document,firstPage);
         pdPageContentStream.beginText();
         pdPageContentStream.setFont(PDType1Font.HELVETICA,18);
@@ -35,7 +38,14 @@ public class WritePDF {
         pdPageContentStream.newLine();
 
         pdPageContentStream.endText();
+//        pdPageContentStream.close();
+
+//        To add image content in pdf
+        PDImageXObject imageXObject = PDImageXObject.createFromFile("E:\\Project Practice\\APACHE PDFBOX\\PracticeTutorial\\berry.jpg",document);
+        pdPageContentStream.drawImage(imageXObject,200,585,160,120);
+
         pdPageContentStream.close();
+
 
         document.save("E:\\Project Practice\\APACHE PDFBOX\\PracticeTutorial\\input.pdf");
         document.close();
